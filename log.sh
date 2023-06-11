02201a4eeb75e113fbac7185d18a942a6fbb1ea1
Yusuf karback <yusufkarback64@gmail.com>
2023-06-12 01:09:39 +0700

    tambah flask

--- log.sh
+++ log.sh
@@ -41,18 +41,22 @@ function cek_cpu(){
 function cek_localhost() {
     local url=$1
     local method=$2
-    local response=$(curl -s -o /dev/null -w "%{http_code}" "$APP_URL")
+    #local response=$(curl -s -o /dev/null -w "%{http_code}" "$APP_URL")
     
-    if [[ $response -eq 200 ]]; then
-        echo "Aplikasi Flask sudah menyala di $APP_URL"
+    SERVICE_NAME='main.py'
+    
+    if ps aux | awk '{print $12}' >/dev/null | grep -q "main.py"; then
+        echo "$(date): Service $SERVICE_NAME is running."
+        curl -s -o /dev/null --data chat_id="-1001880819798" --data-urlencode "$(date): Service $SERVICE_NAME is running." "https://api.telegram.org/bot6282401844:AAGiWIxqy4-3ixDWCD1CVS0cubOCB7fYPuE/sendMessage?parse_mode=HTML"
+        
     else
-        echo "Flask down"
-        echo "starting flask..."
-        python3 main.py
-        echo "flask started"
+        echo "$(date): Service $SERVICE_NAME is not running."
+        curl -s -o /dev/null --data chat_id="-1001880819798" --data-urlencode "$(date): Service $SERVICE_NAME is not running." "https://api.telegram.org/bot6282401844:AAGiWIxqy4-3ixDWCD1CVS0cubOCB7fYPuE/sendMessage?parse_mode=HTML"
+        
     fi
     
-    log_flask "[$method] $url $response"
+    
+    #log_flask "[$method] $url $response"
 }
 
 
@@ -76,7 +80,7 @@ function make_request_404() {
 #cek apakah localhost sudah nyala
 cek_localhost "$APP_URL" "GET"
 
-# cek_cpu
+cek_cpu
 
 # Membuat beberapa permintaan HTTP ke aplikasi Flask sebagai contoh
 make_request "$APP_URL" "GET"
