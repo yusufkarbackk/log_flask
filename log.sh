aa803f1ddb4bca95e8b9eb4931746e2dcdd4d409
Yusuf karback <yusufkarback64@gmail.com>
2023-06-11 10:13:51 +0700

    bikin bot

--- log.sh
+++ log.sh
@@ -13,24 +13,26 @@ function log_flask() {
   local timestamp=$(date +"%Y-%m-%d %T")
   local message=$1
   echo "[$timestamp] $message" >> "$LOG_FILE"
+
+  curl -s -o /dev/null --data chat_id="-1001880819798" --data-urlencode "text=[$timestamp] $message" >> "$LOG_FILE" "https://api.telegram.org/bot6282401844:AAGiWIxqy4-3ixDWCD1CVS0cubOCB7fYPuE/sendMessage?parse_mode=HTML"
 }
 
-function cek_cpu(){
-  threshold=80
+# function cek_cpu(){
+#   threshold=80
 
-# Get the CPU usage percentage using the top command and extract the value
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+# # Get the CPU usage percentage using the top command and extract the value
+# cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 
-# Compare the CPU usage to the threshold
-if (( $(echo "$cpu_usage > $threshold" | bc -l) )); then
-  echo "CPU usage is above the threshold of $threshold%"
-else
-  echo "CPU usage is below the threshold of $threshold%"
-fi
+# # Compare the CPU usage to the threshold
+# if (( $(echo "$cpu_usage > $threshold" | bc -l) )); then
+#   echo "CPU usage is above the threshold of $threshold%"
+# else
+#   echo "CPU usage is below the threshold of $threshold%"
+# fi
 
-log_flask $cpu_usage
+# log_flask $cpu_usage
 
-}
+# }
 
 function cek_localhost() {
   local url=$1
@@ -69,7 +71,7 @@ fi
 #cek apakah localhost sudah nyala
 cek_localhost "$APP_URL" "GET"
 
-cek_cpu
+# cek_cpu
 
 # Membuat beberapa permintaan HTTP ke aplikasi Flask sebagai contoh
 make_request "$APP_URL" "GET"
